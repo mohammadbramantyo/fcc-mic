f00dbb0d6c1125888e892ffecd8434bbab1968b3
bramantyo putra <mohammadbramantyo@gmail.com>
2024-09-20 15:30:03 +0000

    add functional test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,78 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
 
+    this.timeout(5000);
+
+    test("Valid input", function (done) {
+        chai.request(server)
+            .keepOpen()
+            .get('/api/convert?input=10L')
+            .end(function(err,res){
+                assert.equal(res.status,200);
+                assert.equal(res.type, 'application/json');
+                assert.equal(res.body.initNum, 10);
+                assert.equal(res.body.initUnit, 'L');
+                assert.equal(res.body.returnNum, 2.64172);
+                assert.equal(res.body.returnUnit, 'gal');
+                
+                done();
+            });
+    });
+    
+    test("Invalid unit input", function (done) {
+        chai.request(server)
+            .keepOpen()
+            .get('/api/convert?input=32g')
+            .end(function(err,res){
+                assert.equal(res.status,200);
+                assert.equal(res.type, 'text/html');
+                assert.equal(res.text, 'invalid unit');
+                
+                done();
+            });
+    });
+    
+    test("Invalid number input", function (done) {
+        chai.request(server)
+            .keepOpen()
+            .get('/api/convert?input=3/7.2/4kg')
+            .end(function(err,res){
+                assert.equal(res.status,200);
+                assert.equal(res.type, 'text/html');
+                assert.equal(res.text, 'invalid number');
+                
+                done();
+            });
+    });
+    
+    test("Invalid number and unit", function (done) {
+        chai.request(server)
+            .keepOpen()
+            .get('/api/convert?input=3/7.2/4kilomegagram')
+            .end(function(err,res){
+                assert.equal(res.status,200);
+                assert.equal(res.type, 'text/html');
+                assert.equal(res.text, 'invalid number and unit');
+                
+                done();
+            });
+    });
+    
+    test("Convert with no number", function (done) {
+        chai.request(server)
+            .keepOpen()
+            .get('/api/convert?input=kg')
+            .end(function(err,res){
+                assert.equal(res.status,200);
+                assert.equal(res.type, 'application/json');
+                assert.equal(res.body.initNum, 1);
+                assert.equal(res.body.initUnit, 'kg');
+                assert.equal(res.body.returnNum, 2.20462);
+                assert.equal(res.body.returnUnit, 'lbs');
+                
+                done();
+            });
+    });
 });
